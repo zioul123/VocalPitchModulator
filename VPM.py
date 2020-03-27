f9c93e22853c0137af424d93d48bee4c1163eb3f
Zachary Feng <zachary.feng@hotmail.com>
2020-03-27 16:29:14 +0800

    FFT pitch changer (#3)
    * completed simple_ffts_pitch_shift method
    
    * used sample_rate variable instead of 44100

--- VPM.py
+++ VPM.py
@@ -7,6 +7,7 @@ from scipy.io.wavfile import write
 import scipy.signal as sis
 import scipy.fftpack as fftpack
 
+import librosa
 import numpy as np
 
 # Sample rate and bit depth
@@ -15,22 +16,22 @@ short_max = 32768
 
 def create_data_ref_list(file_path, n_pitches, n_vowels, n_people):
     """Create a list of all the filenames in the dataset.
-    
+
     To access the list of files, use data_ref_list[vowel_idx][pitch_idx]
     A specific filename is accessed with data_ref_list[vowel_idx][pitch_idx][person_idx]
-    
+
     Args:
-        filepath (str): The file path to the csv file containing the 
+        filepath (str): The file path to the csv file containing the
             filenames of all .wav files
         n_pitches (int): The number of pitches in our dataset
         n_vowels (int): The number of vowels in our dataset
-        n_people (int): The number of people in our dataset 
+        n_people (int): The number of people in our dataset
 
     Returns:
         data_ref_list (list): A list of filenames, organized by word, then
             pitch, then people (n_vowels, n_pitches, n_people)
     """
-    data_ref_list = [ [ [] for pIdx in range(0, n_pitches) ] 
+    data_ref_list = [ [ [] for pIdx in range(0, n_pitches) ]
                       for wIdx in range(0, n_vowels) ]
 
     with open(file_path) as dataset_csv:
@@ -44,16 +45,16 @@ def create_data_ref_list(file_path, n_pitches, n_vowels, n_people):
 def create_data_label_pairs(n_pitches):
     """Create a dictionary/array of data-label pairs.
 
-    This provides an array of 3-tuples, as well as a dictionary of 
-    arrays, where each subarray contains 3-tuples, with elements: 
+    This provides an array of 3-tuples, as well as a dictionary of
+    arrays, where each subarray contains 3-tuples, with elements:
     [shift_amt, input_pitch_idx, label_pitch_Idx],
     where input_pitch_idx is the input, label_pitch_Idx is the desired output.
 
     This is used so that we can feed data-label pairs to our neural net.
-    We should use each 3-tuple n_people * n_vowels times, with 
+    We should use each 3-tuple n_people * n_vowels times, with
     the files referenced via data_list[vowel_idx][pitch_idx][person_idx],
     and the pitch shift amount provided to the NN.
-    
+
     Args:
         n_pitches (int): The number of pitches in our dataset
 
@@ -63,7 +64,7 @@ def create_data_label_pairs(n_pitches):
         data_label_pairs_dict: A dictionary of dimension n_pitchShifts, where each
             element is an array (n_words * n_startingPitches,), where:
             n_pitchShifts: the number of possible pitch shifts,
-            n_startingPitches: the number of starting pitches for that 
+            n_startingPitches: the number of starting pitches for that
             shift_amt value
     """
     def append_tuple(shift_amt, pitch_idx):
@@ -88,18 +89,18 @@ def create_data_label_pairs(n_pitches):
     for shift_amt in range(-n_pitches + 1, 0):
         for pitch_idx in range(n_pitches - 1, -1 - shift_amt, -1):
             append_tuple(shift_amt, pitch_idx)
-                
+
     return data_label_pairs, data_label_pairs_dict
 
 def load_wav_files(rel_path, data_list):
     """Takes a list of filepaths, and returns a 2D array with all their data.
 
     The function also ensures that the resulting array only contains mono data.
-    
+
     Args:
         rel_path (str): The relative path of the filenames in file_paths.
             i.e. the filepath would be rel_path/file_paths[i]
-        file_paths (list): A list of filenames, where each filename is a 
+        file_paths (list): A list of filenames, where each filename is a
             .wav file to be added to the output.
 
     Returns:
@@ -136,8 +137,8 @@ def compute_hop_length(win_length, overlap):
 # @Rachel/Shaun, this is the whole "Basic Preprocessing" part
 def stft(waveform, win_length=1024, overlap=.5, window='hann'):
     """Takes a waveform and returns a 2D complex-valued matrix (spectrogram).
-    
-    The function performs STFT, i.e. windowing and performing FFT on each 
+
+    The function performs STFT, i.e. windowing and performing FFT on each
     window. This is a wrapper for the librosa.core.stft function.
 
     Args:
@@ -145,7 +146,7 @@ def stft(waveform, win_length=1024, overlap=.5, window='hann'):
         win_length (int): The size of each window (and corresponding FFT)
         overlap (float): The amount of overlap between each window. This
             translates to the hop_length.
-        window (str): The window to use, specified by scipy.signal.get_window. 
+        window (str): The window to use, specified by scipy.signal.get_window.
 
     Returns:
         ffts (np.ndarray): A 2D complex-valued matrix such that
@@ -182,10 +183,10 @@ def istft(ffts, win_length=1024, overlap=.5, window='hann'):
     """
 
 # @Rachel/Shaun This is the "Mel Filter 1" and "Mel Filter 2"
-def ffts_to_mel(ffts, win_length=1024, overlap=.5, n_mels=256, 
+def ffts_to_mel(ffts, win_length=1024, overlap=.5, n_mels=256,
     n_mfcc=20, skip_mfcc=False):
     """Converts a spectrogram to a mel-spectrogram and MFCC.
-    
+
     This function is a wrapper for librosa.feature.melspectrogram and
     librosa.feature.mfcc.
 
@@ -207,12 +208,12 @@ def ffts_to_mel(ffts, win_length=1024, overlap=.5, n_mels=256,
             mel_spectrogram[m, t] is the magnitude of mel bin m at frame t
             The dimensions are (n_mels, [ffts.shape[1]])
         mfcc (np.ndarray): A 2D matrix such that
-            mfcc[m, t] is the magnitude of the mth feature at frame t 
+            mfcc[m, t] is the magnitude of the mth feature at frame t
     """
     """
     !! Write code here !!
     Louiz's note: Please handle sampling rate properly, we assume always 44100.
-    Check out librosa.filters.mel if unsure how to write the arguments to call 
+    Check out librosa.filters.mel if unsure how to write the arguments to call
     librosa.feature.melspectrogram.
     """
     if not skip_mfcc:
@@ -221,21 +222,21 @@ def ffts_to_mel(ffts, win_length=1024, overlap=.5, n_mels=256,
         !! Write code to compute MFCC here !!
         """
 
-# @Zach This is the "Pitch Shift" 
+# @Zach This is the "Pitch Shift"
 def simple_fft_pitch_shift(fft, shift_amt):
     """Takes a single fft vector and shifts all values in the frequency domain.
 
-    This is a "naive" pitch shift that simply up-shifts the values in the 
-    given fft, and is not expected to sound natural. Note that this is done on 
-    a SINGLE time slice. For shifting of an entire spectrogram, use 
+    This is a "naive" pitch shift that simply up-shifts the values in the
+    given fft, and is not expected to sound natural. Note that this is done on
+    a SINGLE time slice. For shifting of an entire spectrogram, use
     simple_ffts_pitch_shift instead.
 
     Explanation:
-    Assume that each value in fft (e.g. fft[f]), is given by a (value, freq) 
-    pair. We shift the pitch by multiplying the frequency values by 
-    (2**(shift_amt/12)), and interpolating the values back into the original 
-    frequency bin values (since the fft bins must keep their original frequency 
-    resolution). 
+    Assume that each value in fft (e.g. fft[f]), is given by a (value, freq)
+    pair. We shift the pitch by multiplying the frequency values by
+    (2**(shift_amt/12)), and interpolating the values back into the original
+    frequency bin values (since the fft bins must keep their original frequency
+    resolution).
 
     Example:
     Assume a frequency resolution of 20Hz, where bin 0: 0Hz, bin 1: 20 Hz etc.
@@ -246,9 +247,9 @@ def simple_fft_pitch_shift(fft, shift_amt):
     So to get the value at 60Hz, we will need to interpolate between z' to z.
 
     Args:
-        fft (np.array): A complex-valued array such that ffts[f] is the 
-            complex number representing the FFT value for the spectrum of freq 
-            bin f. Dimensions are (win_length,), where win_length is the 
+        fft (np.array): A complex-valued array such that ffts[f] is the
+            complex number representing the FFT value for the spectrum of freq
+            bin f. Dimensions are (win_length,), where win_length is the
             number of windows used to generate this fft.
         shift_amt (int): The number of semitones to shift the pitch by. The
             expected range is [-15, 15].
@@ -257,9 +258,12 @@ def simple_fft_pitch_shift(fft, shift_amt):
         shifted_fft (np.array): A (win_length,) array with the shifted fft.
     """
     assert(-15 <= shift_amt and shift_amt <= 15)
-    """
-    !! Write code here !!
-    """
+
+    freqs = librosa.core.fft_frequencies(sample_rate,1024)
+    shifted_freqs = freqs * np.power(2, shift_amt/12)
+    shifted_fft = np.interp(freqs, shifted_freqs, fft)
+
+    return shifted_fft
 
 def simple_ffts_pitch_shift(ffts, shift_amt):
     """Takes a 2D spectrogram, and pitch_shifts each time slice.
@@ -274,6 +278,6 @@ def simple_ffts_pitch_shift(ffts, shift_amt):
     Returns:
         shifted_ffts (np.ndarray): A spectrogram of equal dimensions to ffts,
             with shifted frequency space.
-    """ 
+    """
     assert(-15 <= shift_amt and shift_amt <= 15)
     return np.array([ simple_fft_pitch_shift(fft, shift_amt) for fft in ffts.T ]).T
